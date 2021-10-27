c028a0205e935cd69172297c9911093461ccd69b
ziadbadwy <ziadzamab@gmail.com>
2021-10-27 02:16:34 +0200

    final eng

---
+++ javascript/main.js
@@ -0,0 +1,70 @@
+let navBar = document.getElementById('navbarSupportedContent');
+new WOW().init();
+   /*/////////////////////-----------BURGER--------////////////////*/
+   const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    $('.collapse').slideToggle();
+    
+
+
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    $('.collapse').slideToggle();
+    menuOpen = false;
+  }
+});
+/*wndow*/
+$(window).scroll(function(){
+    if($(window).scrollTop() >0) {
+        $('.start').css(            
+            'display',"none"
+
+        );
+        $('.navbar').css(
+            'top',"0px"
+        );
+    }else{
+        $('.start').css(
+            'display',"block"
+
+
+        );
+        $('.navbar').css(
+            'top',"25px"
+        );
+
+    }
+    
+});
+
+
+
+
+
+$(document).ready(function() {
+
+	/* This is basic - uses default settings */
+	
+	$("a#single_image").fancybox();
+	
+	/* Using custom settings */
+	
+	$("a#inline").fancybox({
+		'hideOnContentClick': true
+	});
+
+	/* Apply fancybox to multiple items */
+	
+	$("a.group").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false
+	});
+	
+});
